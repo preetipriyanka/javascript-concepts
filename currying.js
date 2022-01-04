f2a6f8815a343b9f3b8f34404dfa7e494bf10f6a
preetipriyanka24 <preetipriyanka24@gmail.com>
2022-01-04 11:22:04 +0530

    initial commit

---
+++ currying.js
@@ -0,0 +1,15 @@
+function curry(f){
+    return function(a) {
+        return function (b) {
+            return f(a,b);
+        }
+    }
+}
+
+function sum(a,b) {
+    return a+b;
+}
+
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2));
