d0d8dc77af005cd48f97b9fedd6594a68fec46f8
preetipriyanka24 <preetipriyanka24@gmail.com>
2022-01-25 12:48:14 +0530

    Activity Selection

--- Algorithms/ActivitySelection.js
+++ Algorithms/ActivitySelection.js
@@ -1,21 +1,18 @@
-function printMaxActivities(s, f, n) {
+function printMaxActivities(start, finish) {
+  console.log("Following Activities are selected: ");
   let i, j;
-  console.log("Following activities are selected : n");
 
-  // The first activity always gets selected
   i = 0;
   console.log(i + " ");
 
-  // Consider rest of the activities
-  for (j = 1; j < n; j++) {
-    if (s[j] >= f[i]) {
-      console.log(j + " ");
+  for (j = 1; j < start.length; j++) {
+    if (start[j] >= finish[i]) {
+      console.log(start[j]);
       i = j;
     }
   }
 }
 
-let s = [1, 3, 0, 5, 8, 5];
-let f = [2, 4, 6, 7, 9, 9];
-let n = s.length;
-printMaxActivities(s, f, n);
+let start = [1, 3, 0, 5, 8, 5];
+let finish = [2, 4, 6, 7, 9, 9];
+printMaxActivities(start, finish);
