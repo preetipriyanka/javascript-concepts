7ea01cf030a9cf2f7cab4199626599199394be83
Priyanka Kumari <50628773+preetipriyanka@users.noreply.github.com>
2022-05-19 16:38:24 +0530

    Update JumpGame.js

---
+++ data-structures/Array/JumpGame.js
@@ -0,0 +1,11 @@
+//Description:
+//Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+
+//Each element in the array represents your maximum jump length at that position.
+
+//Determine if you are able to reach the last index.
+
+//Solution:
+//Time Complexity : O(n)
+//Space Complexity: O(1)
+
